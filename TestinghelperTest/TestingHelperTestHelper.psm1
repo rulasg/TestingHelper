cd675e03fd29a17286a116e4f647d4ef746be697
raulg <raul.gonzalez@wizeline.com>
2021-06-09 13:43:26 +0200

    Optimize PSModulePath

--- TestinghelperTest/TestingHelperTestHelper.psm1
+++ TestinghelperTest/TestingHelperTestHelper.psm1
@@ -1,16 +1,20 @@
 
 Write-Host "Loading TestingHelperTestHelper ..." -ForegroundColor DarkCyan
 
+# Windows and MacOS uses different separatod char on PsModulePath
+$splitchar = ($Env:OS -eq 'Windows_NT') ? ';' : ':'
+
 function Add-PSModulePath {
     param (
         [parameter(ValueFromPipeline)][string] $Path
     )
+
+    $env:PSModulePath+= ("$splitChar{0}" -f ( Resolve-Path -Path:$Path ))
     
-    # Windows and MacOS uses different separatod char on PsModulePath
-    if ($Env:OS -eq 'Windows_NT') {
-        $env:PSModulePath+= (';{0}' -f ( Resolve-Path -Path:$Path ))
-    }
-    else {
-        $env:PSModulePath+= (':{0}' -f ( Resolve-Path -Path:$Path ))
-    }
 } Export-ModuleMember -Function Add-PSModulePath
+
+function Get-PSModulePath {
+    
+    $env:PSModulePath.Split($splitchar)
+
+} Export-ModuleMember -Function Get-PSModulePath
