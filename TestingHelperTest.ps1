776ae9d2774d255303a70d495b495589ef4bff6b
Raúl González <rulasg@github.com>
2023-05-04 07:59:07 +0000

    Bug: test run on codespaces

--- TestingHelperTest.ps1
+++ TestingHelperTest.ps1
@@ -1,13 +1,22 @@
 [CmdletBinding()]
 param ()
 
+function Import-TestingHelper($Version){
+
+    if (-not (import-Module TestingHelper -RequiredVersion $Version -PassThru -ErrorAction SilentlyContinue )) {
+        Install-Module -Name TestingHelper -Force -RequiredVersion $Version
+        Import-Module -Name TestingHelper -Force -RequiredVersion $Version
+    }
+}
 
 # As we are testing TestingHelper from TestingHelper we need to differentiate the Testing with the Tested versions
-# New Test-ModulelocalPSD1 that will find the tested version. This way this script will call the testing version of TestingHelper. 
+# Using new Test-ModulelocalPSD1 that will find and load the tested version on the root folder.
+# Using Import-Module to import a released teating version of TestingHelper
 
 # As we are using Test-ModulelocalPSD1 first present on this version of the module we use testing with tested.
-# We need to import the tested from here. This need to be removed on next version where testing has this function
+# We need to import the tested from here. 
+# This need to be removed on next version where testing has this function
 Import-Module -Name ./TestingHelper.psd1 -Force
-Import-Module -Name TestingHelper
+Import-TestingHelper -Version 2.0
 
 Test-ModulelocalPSD1
