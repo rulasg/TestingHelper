e5e8845316266a589c94714d0bf8d6deade0460b
rulasg <rulasg@outlook.com>
2023-06-16 14:03:46 +0200

    sync.ps1 improvement

--- sync.ps1
+++ sync.ps1
@@ -1,6 +1,6 @@
 <#
 .SYNOPSIS
-    Synchronizes TestingHelper templates wiles
+    Synchronizes TestingHelper templates files
 
 .DESCRIPTION
     Synchronizes TestingHelper templates to the local repo.
@@ -10,33 +10,12 @@
     https://raw.githubusercontent.com/rulasg/DemoPsModule/main/sync.ps1
 #>
 
-[cmdletbinding(SupportsShouldProcess, ConfirmImpact='High')]
+[cmdletbinding()]
 param()
 
-function Get-UrlContent {
-    [cmdletbinding()]
-    param(
-        [Parameter(Mandatory=$true)][string]$url
-    )
-    $wc = New-Object -TypeName System.Net.WebClient
-    $fileContent = $wc.DownloadString($url)
+. ($PSScriptRoot | Join-Path -ChildPath "sync-Helper.ps1")
 
-    return $fileContent
-}
-
-function Out-ContentToFile {
-    [cmdletbinding(SupportsShouldProcess)]
-    param(
-        [Parameter(ValueFromPipeline)][string]$content,
-        [Parameter(Mandatory=$true)][string]$filePath
-    )
-
-    if ($PSCmdlet.ShouldProcess($filePath, "Save content [{0}] to file" -f $content.Length)) {
-        $content | Out-File -FilePath $filePath -Force
-    }
-}
-
-Get-UrlContent -Url 'https://raw.githubusercontent.com/rulasg/DemoPsModule/main/test.ps1'            | Out-ContentToFile -FilePath 'test.ps1'
-Get-UrlContent -Url 'https://raw.githubusercontent.com/rulasg/DemoPsModule/main/release.ps1'         | Out-ContentToFile -FilePath 'release.ps1'
-Get-UrlContent -Url 'https://raw.githubusercontent.com/rulasg/DemoPsModule/main/publish.ps1'         | Out-ContentToFile -FilePath 'publish.ps1'
-Get-UrlContent -Url 'https://raw.githubusercontent.com/rulasg/DemoPsModule/main/publish-Helper.ps1'  | Out-ContentToFile -FilePath 'publish-Helper.ps1'
+Save-UrlContentToFile -Url 'https://raw.githubusercontent.com/rulasg/DemoPsModule/main/test.ps1'            -FilePath 'test.ps1'
+Save-UrlContentToFile -Url 'https://raw.githubusercontent.com/rulasg/DemoPsModule/main/release.ps1'         -FilePath 'release.ps1'
+Save-UrlContentToFile -Url 'https://raw.githubusercontent.com/rulasg/DemoPsModule/main/publish.ps1'         -FilePath 'publish.ps1'
+Save-UrlContentToFile -Url 'https://raw.githubusercontent.com/rulasg/DemoPsModule/main/publish-Helper.ps1'  -FilePath 'publish-Helper.ps1'
