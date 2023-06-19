5d707422d31c655e19d8d77820be916b89d55f76
rulasg <rulasg@outlook.com>
2023-06-19 15:35:51 +0200

    Move Assert-ContainsPattern to private function

--- TestingHelperTest/public/Tests-publish.ps1
+++ TestingHelperTest/public/Tests-publish.ps1
@@ -204,44 +204,6 @@ function Assert-Publish_PS1_Invoke-PublishModule{
 
 }
 
-function Assert-ContainsPattern{
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory)] [string] $Expected,
-        [Parameter(Mandatory)] [string[]] $Presented,
-        [Parameter()] [string] $Comment
-    )
-
-    $found = $false
-    foreach($p in $Presented){
-        if ($p -like $Expected) {
-            $found = $true
-            break
-        }
-    }
-
-    Assert-IsTrue -Condition $found -Comment "Not found pattern [$Expected] in $Presented - $Comment "
-}
-
-function Assert-ContainsNotPattern{
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory)] [string] $Expected,
-        [Parameter(Mandatory)] [string[]] $Presented,
-        [Parameter()] [string] $Comment
-    )
-
-    $found = $false
-    foreach($p in $Presented){
-        if ($p -like $Expected) {
-            $found = $true
-            break
-        }
-    }
-
-    Assert-IsFalse -Condition $found -Comment "Found pattern [$Expected] in $Presented - $Comment"
-}
-
 function Assert-Manifest{
     [CmdletBinding()]
     param(
