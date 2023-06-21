b6bec02e2e4c2843024105585167683734204851
rulasg <rulasg@outlook.com>
2023-06-21 09:48:27 +0200

    type on sync.ps1

--- sync.ps1
+++ sync.ps1
@@ -13,9 +13,9 @@
 [cmdletbinding()]
 param()
 
-. ($PSScriptRoot | Join-Path -ChildPath "sync-Helper.ps1")
+. ($PSScriptRoot | Join-Path -ChildPath "sync-helper.ps1")
 
 Save-UrlContentToFile -Url 'https://raw.githubusercontent.com/rulasg/DemoPsModule/main/test.ps1'            -FilePath 'test.ps1'
 Save-UrlContentToFile -Url 'https://raw.githubusercontent.com/rulasg/DemoPsModule/main/release.ps1'         -FilePath 'release.ps1'
 Save-UrlContentToFile -Url 'https://raw.githubusercontent.com/rulasg/DemoPsModule/main/publish.ps1'         -FilePath 'publish.ps1'
-Save-UrlContentToFile -Url 'https://raw.githubusercontent.com/rulasg/DemoPsModule/main/publish-Helper.ps1'  -FilePath 'publish-Helper.ps1'
+Save-UrlContentToFile -Url 'https://raw.githubusercontent.com/rulasg/DemoPsModule/main/publish-helper.ps1'  -FilePath 'publish-helper.ps1'
