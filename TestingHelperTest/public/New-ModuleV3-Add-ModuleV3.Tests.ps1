d06636d509d45d62c6458a614fbf9a479111eccc
rulasg <rulasg@outlook.com>
2023-10-04 20:43:57 +0200

    Fix bug for adding PreRelease to injected function.

--- TestingHelperTest/public/New-ModuleV3-Add-ModuleV3.Tests.ps1
+++ TestingHelperTest/public/New-ModuleV3-Add-ModuleV3.Tests.ps1
@@ -2,33 +2,39 @@
 function TestingHelperTest_NewModuleV3_AddModule_FailCall_NewModuleManifest {
 
     # test when failing calling dependency Microsoft PowerShell.Core/New-ModuleManifest
-
-    $modulename = "MyModule"
     
-    # Inject depepdency
-    $module = Import-Module -Name $TESTED_MANIFEST_PATH -Prefix "LL_" -Force -PassThru
-    & $module {
-        . {
-            function script:New-MyModuleManifest {
-                [CmdletBinding()]
-                param(
-                    [Parameter(Mandatory)][string]$Path,
-                    [Parameter(Mandatory)][string]$RootModule
-                )
-                throw "Injected error from New-MymoduleManifest."
+    try {
+        
+        $modulename = "MyModule"
+        
+        # Inject depepdency
+        $module = Import-Module -Name $TESTED_MANIFEST_PATH -Prefix "LL_" -Force -PassThru
+        & $module {
+            . {
+                function script:New-MyModuleManifest {
+                    [CmdletBinding()]
+                    param(
+                        [Parameter(Mandatory)][string]$Path,
+                        [Parameter(Mandatory)][string]$RootModule,
+                        [Parameter(Mandatory)][string]$PreRelease
+                        )
+                        throw "Injected error from New-MymoduleManifest."
+                    }
+                }
             }
+            
+            $result = Add-LL_ModuleV3 -Name $modulename  @ErrorParameters
+            
+            Assert-IsNull -Object $result -Comment "No module is created"
+            Assert-IsNotNull -Object $errorVar.Exception -Comment "Error is thrown"
+            Assert-Contains -Expected "Injected error from New-MymoduleManifest." -Presented ($errorVar.Exception.Message)
+            Assert-ContainsPattern -Expected "Error creating the PSD1 file.*" -Presented ($errorVar.Exception.Message)
         }
-    }
+        finally {
+            # reset module
+            Import-Module -Name $TESTED_MANIFEST_PATH -Prefix "TT_" -Force
+        }
 
-    $result = Add-LL_ModuleV3 -Name $modulename  @ErrorParameters
-
-    Assert-IsNull -Object $result -Comment "No module is created"
-    Assert-IsNotNull -Object $errorVar.Exception -Comment "Error is thrown"
-    Assert-Contains -Expected "Injected error from New-MymoduleManifest." -Presented ($errorVar.Exception.Message)
-    Assert-ContainsPattern -Expected "Error creating the PSD1 file.*" -Presented ($errorVar.Exception.Message)
-
-    # reset module
-    Import-Module -Name $TESTED_MANIFEST_PATH -Prefix "TT_" -Force
 }
 
 function TestingHelperTest_NewModuleV3_AddModule_DefaultManifest {
